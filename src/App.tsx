1dd24ef9faff483cd77fb66e6d370c813b5d605a
sangwoo <hdsk1234@naver.com>
2025-12-31 07:20:27 +0900

    navbar 여백 수정

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { onAuthStateChanged, User } from "firebase/auth";
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import './App.css'
 
 
 function App() {
@@ -21,30 +22,7 @@ function App() {
     return () => unsubscribe(); // 언마운트 시 구독 해제
   }, []);
 
-  if (loading) return <div>Loading...</div>;
-
-  // return (
-  //   <Router>
-  //     <Navbar />
-  //     <Routes>
-  //       {/* 1. 로그인 상태일 때만 접근 가능한 경로 */}
-  //       <Route 
-  //         path="/" 
-  //         element={user ? <Home /> : <Navigate to="/auth" replace />} 
-  //       />
-
-  //       {/* 2. 로그아웃 상태일 때만 접근 가능한 경로 (로그인 페이지) */}
-  //       <Route 
-  //         path="/auth" 
-  //         element={!user ? <Auth /> : <Navigate to="/" replace />} 
-  //       />
-
-  //       {/* 3. 기타 정의되지 않은 경로는 홈으로 리다이렉트 */}
-  //       <Route path="*" element={<Navigate to="/" replace />} />
-  //     </Routes>
-  //   </Router>
-  // );
-
+  if (loading) return <h1>Loading...</h1>;
   
   return (
    <Router>
